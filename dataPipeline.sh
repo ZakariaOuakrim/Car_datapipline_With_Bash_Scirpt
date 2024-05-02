b1c02e4254bde8c3cbb87f186604c364c28218c5
ZakariaOuakrim <zakarialukyguy@gmail.com>
2024-05-02 17:46:56 +0100

    first commit

---
+++ dataPipeline.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# jm3 data mn user  select where 
+
+# jm3 data mn car  selec where 
+
+#jm3 data mn stationement select 
+
+#clean data  ms7 dakchi column khawi 
+
+# transform data  date year month day time -> month day 
+
+#di dakchi lcsv -> .csv 
+
+
+
+
+
